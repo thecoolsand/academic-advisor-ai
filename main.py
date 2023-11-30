0370d162814235715ef1a97ec8ed66e11f0d28ab
StarTwinkies <krittika.saha.dev@gmail.com>
2023-11-30 12:53:13 +0530

    grading and returning without subject input

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
 import streamlit as st
 from PyPDF2 import PdfReader
 import re
-import os
-from dotenv import load_dotenv
-from getpass import getpass
-
+import requests
 from langchain.chains import LLMChain
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 
 
-# load_dotenv()
+# url = "https://a5c5687a-c49e-4b26-8d5e-7e65812495e1.mock.pstmn.io/getans"
+# response = requests.get(url)
 
 # ---CONSTANTS----
 OPEN_AI_API_KEY = "YOUR_API_KEY_HERE"
 
 # ---CONSTANTS----
 
-q_a_dict_ai = {}
 q_a_dict_student = {}
 
 template = """Question: {question}
@@ -25,11 +22,9 @@ template = """Question: {question}
 Answer: Let's think step by step."""
 
 prompt = PromptTemplate(template=template, input_variables=["question"])
-llm = OpenAI(api_key=OPEN_AI_API_KEY)
+llm = OpenAI(api_key=OPEN_AI_API_KEY, max_tokens=1500)
 
 llm_chain = LLMChain(prompt=prompt, llm=llm)
-question = ""
-
 
 
 def get_pdf_text(pdf_docs):
@@ -50,23 +45,31 @@ def home():
     with st.sidebar:
         st.subheader("Your documents")
         pdf_docs = st.file_uploader("Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+    subject = st.radio(label="Choose your subject", options=["Physics", "Chemistry", "Biology", "Articial Intelligence (Code 417)", "Information Technology (Code 402)"])
     if st.button("Process"):
         with st.spinner("Processing..."):
-            raw_text = get_pdf_text(pdf_docs)
+            raw_text = [i.replace("\n", " ") for i in get_pdf_text(pdf_docs)]
             questions_student = []
             answers_student = []
             for i in raw_text:
-                text_chunks = re.split('\d+' + "." + "\n", i)
+                text_chunks = re.split('\d+' + "." + " ", i)
                 text_chunks.pop(0)
-                text_chunks = [i.replace("\n", " ") for i in text_chunks]
+                # print(text_chunks)
                 if raw_text.index(i) == 0:
                     for j in text_chunks:
                         answers_student.append(j)
                 else:
                     for j in text_chunks:
                         questions_student.append(j)
+            if subject == "Physics":
+                    pass
             for i in questions_student:
                 q_a_dict_student[i] = answers_student[questions_student.index(i)]
+            print(llm_chain.run(f"Grade these answers to their respective questions one by one (check with reference "
+                                f"to CBSE Class 9 Physics syllabus) with a little leniency and assign each one of "
+                                f"them a percentage based on how correct they are and format the whole data along "
+                                f"with the explanation of grades into a convenient JSON format: {q_a_dict_student}"))
+
         st.write(q_a_dict_student)
 
 
